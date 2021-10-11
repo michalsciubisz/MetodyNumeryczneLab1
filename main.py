aa56c8c47ecdcd49004a77bca107f8c4dc481ef1
michalsciubisz <michalsci@student.agh.edu.pl>
2021-10-11 23:12:51 +0200

    finished
    dodano komentarze do poszczegolnych zadan

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ def cylinder_area(r: float,h: float):
         return 2 * r * r * math.pi + 2 * r * h * math.pi
     else:
         return np.NaN
+    """
+    powyzsza bardzo prosta funkcja, wykorzystuje pi z biblioteki math oraz dziala tylko i wylacznie w przypadku podania poprawnych wartosci w wywolaniu funkcji
+    """
 
 def fib(n:int):
     """Obliczenie pierwszych n wyrazów ciągu Fibonnaciego. 
@@ -30,17 +33,17 @@ def fib(n:int):
     """
     list = [0]
     a_adv, b_adv = 0, 1
-    if n < 0 or not isinstance(n, int):
+    if n < 0 or not isinstance(n, int): #sprawdzam czy wprowadzony zostal poprawny argument
         return None
     elif n == 0:
         pass
-    elif n == 1:
+    elif n == 1: #ustalenie poszczegolnych warunkow przy podaniu jako argument 1 lub 0
         list.append(1)
     else:
-        for _ in range(0, n):
+        for _ in range(0, n): #wlasciwa funkcja ktora oblicza element ciagu fibonaciego oraz dodaje ja do listy
             a_adv, b_adv= b_adv, a_adv + b_adv
             list.append(a_adv) 
-    return np.array(list)
+    return np.array(list) #zwracana jest wartosc jako przeksztalcona lista  w array
 
 def matrix_calculations(a:float):
     """Funkcja zwraca wartości obliczeń na macierzy stworzonej 
@@ -56,12 +59,12 @@ def matrix_calculations(a:float):
     """
     M = np.array([[a,1,-a],[0,1,1],[-a,a,1]])
     Mt = np.transpose(M)
-    Mdet = np.linalg.det(M)
-    if Mdet == 0:
+    Mdet = np.linalg.det(M) #wykorzystanie gotowych funkcji z biblioteki numpy
+    if Mdet == 0: #sprawdzenie czy istnieje macierz odwrotna
         return np.NaN
     else:
         Minv = np.linalg.inv(M)
-        return (Minv, Mt, Mdet)
+        return (Minv, Mt, Mdet) #zwrocenie otrzymanych wynikow w postaci krotki, mozna zapisac ja bez nawiasow
 
 
 def custom_matrix(m:int, n:int):
@@ -75,10 +78,10 @@ def custom_matrix(m:int, n:int):
     Returns:
     np.ndarray: macierz zgodna z opisem z zadania 7.
     """
-    Matrix = np.zeros((m,n))
-    
-    for a in range(0,n):
-        for b in range(0,m):
+    Matrix = np.zeros((m,n)) #stworzenie macierzy o zadanych rozmiarach, ktora zostanie uzupelniona   
+
+    for a in range(0,n): #utworzone dwie petle ktore nastepnie wypelniaja odpowiednio kazdy element macierzy, ten sposob wymaga wykorzystania wiekszej mocy obliczeniowej poniewaz 
+        for b in range(0,m): #funkcja w kazdej petli spradza warunek if
             if m > n:
                 Matrix[b,a] = b
             else:
